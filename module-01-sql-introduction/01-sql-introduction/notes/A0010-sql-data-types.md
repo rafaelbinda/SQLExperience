9f951f233452401b15aa4b6bc059f55e542f77da
rafaelbinda <rafabinda@gmail.com>
2026-02-28 20:15:28 -0300

    docs: add study notes on SQL Data Types lab: add SQL Data Types examples

---
+++ module-01-sql-introduction/01-sql-introduction/notes/A0010-sql-data-types.md
@@ -0,0 +1,19 @@
+﻿# A0010 – Sql Data Types 
+
+> **Author:** Rafael Binda  
+> **Created:** 2026-02-28  
+> **Version:** 1.0 
+
+---
+
+## Descrição
+Este documento apresenta os tipos de Dados no SQL Server
+
+---
+
+## Observações
+`scripts\Q0005-sql-data-types.sql`  
+
+---
+
+## 1 - Strings
---
+++ module-01-sql-introduction/01-sql-introduction/scripts/Q0005-sql-data-types.sql
@@ -0,0 +1,18 @@
+﻿/*
+===============================================================================
+Author      : Rafael Binda
+Created     : 2026-02-28
+Version     : 1.0
+Task        : Q0005 - SQL data types
+Databases   : ExamplesDB
+Object      : Script
+Description : Examples demonstrating data types in SQL Server. 
+Notes       : A0010-sql-data-types.md
+===============================================================================
+*/
+
+SET NOCOUNT ON;
+GO 
+ 
+USE ExamplesDB;
+GO
