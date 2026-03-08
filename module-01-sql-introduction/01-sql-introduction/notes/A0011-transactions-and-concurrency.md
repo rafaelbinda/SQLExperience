5e540500995dedd5d71c9fe8d637b9feaab8babd
rafaelbinda <rafabinda@gmail.com>
2026-03-07 21:25:14 -0300

    docs: add initial transactions and concurrency documentation and script

---
+++ module-01-sql-introduction/01-sql-introduction/notes/A0011-transactions-and-concurrency.md
@@ -0,0 +1,20 @@
+﻿# A0011 – Transactions and Concurrency
+> **Author:** Rafael Binda  
+> **Created:** 2026-03-07  
+> **Version:** 1.0 
+
+---
+
+## Descrição
+Este documento apresenta informações a respeito de transações e concorrência no SQL Server
+
+---
+
+## Hands-on  
+[Transactions and Concurrency ](../scripts/Q0009-transactions-and-concurrency.sql)  
+
+
+---
+
+
+
---
+++ module-01-sql-introduction/01-sql-introduction/scripts/Q0009-transactions-and-concurrency.sql
@@ -0,0 +1,18 @@
+﻿/*
+===============================================================================
+Author      : Rafael Binda
+Created     : 2026-03-07
+Version     : 1.0
+Task        : Q0009-transactions-and-concurrency.sql
+Databases   : ExamplesDB 
+Object      : Script
+Description : Examples demonstrating transactions and concurrency SQL Server
+Notes       : A0011-transactions-and-concurrency.md
+===============================================================================
+*/
+
+SET NOCOUNT ON;
+GO 
+ 
+USE ExamplesDB;
+GO
